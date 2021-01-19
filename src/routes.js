c7125dedc506974d02e533aca75cba773454a57a
Wilhelm <va3093@gmail.com>
2021-01-19 13:26:18 +0000

    added log

--- src/routes.js
+++ src/routes.js
@@ -6,7 +6,7 @@ const routes = {
         res.send('Hey 👋');
     },
     postEvents: (req, res) => {
-        console.log(req)
+        console.log("Request:", req)
         let event;
         let project;
         if (req.headers['Sentry-Hook-Signature'] !== undefined) {
